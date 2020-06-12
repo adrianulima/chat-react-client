a24ed6dd7d25fb62fc0dbf6ae2a87713a5b667b5
junilima <junim3571100>
2020-06-12 05:46:00 -0300

    Implemented ChatWindowHeader, ChatWindowBody, ChatMessagesContainer and ChatUsersContainer

--- example/src/Chat.js
+++ example/src/Chat.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
-import { ChatButton, ChatWindow, ChatWrapper } from 'chat-react-client'
+import {
+  ChatButton,
+  ChatWindow,
+  ChatWindowHeader,
+  ChatWindowBody,
+  ChatWrapper,
+  ChatMessagesContainer,
+  ChatUsersContainer,
+} from 'chat-react-client'
 import { BsChatDotsFill, BsXCircleFill } from 'react-icons/bs'
+import { Button } from 'reactstrap'
 
 const Chat = () => {
   const [open, setOpen] = useState(false)
   const [disabled, setDisabled] = useState(false)
+  const [showMessages, setShowMessages] = useState(true)
 
   return (
     <div>
@@ -25,7 +35,53 @@ const Chat = () => {
             <BsChatDotsFill size="20px" />
           )}
         </ChatButton>
-        {open && <ChatWindow>CHAT</ChatWindow>}
+        {open && (
+          <ChatWindow>
+            <ChatWindowHeader
+              style={{
+                padding: '10px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}>
+              Chat Window
+              <Button
+                color="secondary"
+                onClick={() => {
+                  setShowMessages(!showMessages)
+                }}>
+                {showMessages ? 'Show Users' : 'Show Messages'}
+              </Button>
+            </ChatWindowHeader>
+            <ChatWindowBody>
+              {showMessages ? (
+                <ChatMessagesContainer
+                  messages={[
+                    {
+                      text: 'mensagem1',
+                      timestamp: '10000000',
+                      userId: '1',
+                      userName: 'Ciclano',
+                    },
+                    {
+                      text: 'mensagem2',
+                      timestamp: '20000000',
+                      userId: '2',
+                      userName: 'Fulano',
+                    },
+                  ]}
+                />
+              ) : (
+                <ChatUsersContainer
+                  users={[{ userId: '1', name: 'Ciclano' }]}
+                  onClickUser={(user) => {
+                    return user
+                  }}
+                />
+              )}
+            </ChatWindowBody>
+          </ChatWindow>
+        )}
       </ChatWrapper>
     </div>
   )
---
+++ src/components/ChatWindowBody.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../styles.css'
+
+const ChatWindowBody = (props) => {
+  return <div className={styles['chat-window-body']} {...props} />
+}
+
+export default ChatWindowBody
