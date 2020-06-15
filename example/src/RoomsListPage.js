e759618db390a098633bad44199b068f23675872
Adriano Lima <adrianulima@hotmail.com>
2020-06-15 20:57:40 -0300

    Code cleanup and rooms mock update

--- example/src/RoomsListPage.js
+++ example/src/RoomsListPage.js
@@ -1,15 +1,6 @@
 import React, { useState } from 'react'
 import PageContainer from './PageContainer'
-import {
-  Row,
-  Col,
-  Button,
-  Card,
-  CardTitle,
-  CardBody,
-  CardText,
-  CardFooter,
-} from 'reactstrap'
+import { Row, Col, Button, Card, CardBody, CardText } from 'reactstrap'
 import {
   BsFillLockFill,
   BsFillUnlockFill,
@@ -21,13 +12,13 @@ import ModalDeleteRoom from './components/modals/ModalDeleteRoom'
 import { map } from 'lodash'
 
 const rooms = [
-  { roomId: 1234, size: 6, users: ['1', '2'], protected: true },
-  { roomId: 2345, size: 3, users: ['3'], protected: true },
-  { roomId: 3456, size: 4, users: ['4'] },
-  { roomId: 4567, size: 6, users: [] },
-  { roomId: 5678, size: 6, users: [] },
-  { roomId: 6789, size: 6, users: [] },
-  { roomId: 7890, size: 6, users: [] },
+  { roomId: 1234, size: 16, usersCount: 6, protected: true },
+  { roomId: 2345, size: 4, usersCount: 2, protected: true },
+  { roomId: 3456, size: 4, usersCount: 0 },
+  { roomId: 4567, size: 8, usersCount: 2 },
+  { roomId: 5678, size: 12, usersCount: 6 },
+  { roomId: 6789, size: 4, usersCount: 0, protected: true },
+  { roomId: 7890, size: 16, usersCount: 10 },
 ]
 
 const RoomsListPage = () => {
@@ -128,7 +119,7 @@ const RoomsListPage = () => {
                 </CardBody>
                 <Row className="p-1 pl-2">
                   <Col className="d-flex align-items-center">
-                    Users: {room.users.length}/{room.size}
+                    Users: {room.usersCount}/{room.size}
                   </Col>
                   <Col className="text-right">
                     <Button
